99a39c6e1b64bec19aaa50c3453de9530f614363
phamducdat <phamducdat2402@gmail.com>
2023-10-18 15:54:09 +0700

    feat: using transactional

--- src/main/java/com/datpd/checkin/service/UserService.java
+++ src/main/java/com/datpd/checkin/service/UserService.java
@@ -1,14 +1,18 @@
 package com.datpd.checkin.service;
 
 import com.datpd.checkin.dto.UserDto;
+import com.datpd.checkin.entity.TurnHistoryEntity;
 import com.datpd.checkin.entity.UserEntity;
 import com.datpd.checkin.mapper.UserMapper;
+import com.datpd.checkin.repository.TurnHistoryRepository;
 import com.datpd.checkin.repository.UserRepository;
 import com.datpd.checkin.util.CheckInService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -17,6 +21,7 @@ public class UserService {
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
+    private final TurnHistoryRepository turnHistoryRepository;
 
     private final TurnHistoryService turnHistoryService;
     private final UserMapper mapper;
@@ -24,10 +29,12 @@ public class UserService {
     private final CheckInService checkInService;
 
     public UserService(UserRepository userRepository,
+                       TurnHistoryRepository turnHistoryRepository,
                        TurnHistoryService turnHistoryService,
                        UserMapper mapper,
                        CheckInService checkInService) {
         this.userRepository = userRepository;
+        this.turnHistoryRepository = turnHistoryRepository;
         this.turnHistoryService = turnHistoryService;
         this.mapper = mapper;
         this.checkInService = checkInService;
@@ -38,6 +45,7 @@ public class UserService {
         return optionalUserEntity.map(mapper::mapFromEntityToDto).orElse(null);
     }
 
+    @Transactional
     public UserDto checkInByUserId(long userId) {
         Optional<UserEntity> optionalUserEntity = userRepository.findById(userId);
         if (optionalUserEntity.isPresent()) {
@@ -51,10 +59,14 @@ public class UserService {
                 userRepository.save(userEntity);
 
                 // Create history
-                turnHistoryService.createTurnHistory(
-                        userEntity.getId(),
-                        1L,
-                        balance);
+                logger.info("Create Turn History");
+                TurnHistoryEntity turnHistoryEntity = new TurnHistoryEntity();
+                turnHistoryEntity.setUserId(userId);
+                turnHistoryEntity.setAmount(1);
+                turnHistoryEntity.setBalance(balance);
+                turnHistoryEntity.setCreateAt(new Date());
+                turnHistoryRepository.save(turnHistoryEntity);
+
             }
             return mapper.mapFromEntityToDto(userEntity);
         } else
